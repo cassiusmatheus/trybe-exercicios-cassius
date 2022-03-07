12cc15db7501095bb802360227ef13d8cbbfcc0b
Cassius Matheus <cassiusmatheus.soares@gmail.com>
2022-03-07 20:27:45 -0300

    programa para escolher maior numero de 3 variaveis

---
+++ fundamentos/bloco-04-intruducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/passo3.js
@@ -0,0 +1,13 @@
+const a = 30;
+const b = 30;
+const c = 50;
+
+if (a>=b && a>=c) {
+    console.log (a + " é o maior número");
+}
+else if (b >= c && b >= a) {
+console.log (b + " é o maior número");
+}
+else if (c >= a && c >= b) {
+    console.log (c + " é o maior número");
+}
