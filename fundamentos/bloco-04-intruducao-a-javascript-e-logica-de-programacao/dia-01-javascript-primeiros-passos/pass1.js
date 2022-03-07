2dbdef39f5c5385e694d5eae4be092960706a58c
Cassius Matheus <cassiusmatheus.soares@gmail.com>
2022-03-07 18:38:33 -0300

    passo 1 programa com operações de duas variáveis

---
+++ fundamentos/bloco-04-intruducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/pass1.js
@@ -0,0 +1,11 @@
+// Passo 1
+// Declaração das variáveis
+let a = 2560;
+let b = 2;
+//  impressão das operações com as variáveis em ordem: 
+// soma, subtração, exponenciação, divisão e resto de divisão
+console.log (a + b);
+console.log (a-b);
+console.log (a*b);
+console.log(a/b)
+console.log (a%b)
