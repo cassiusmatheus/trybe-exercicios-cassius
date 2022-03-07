e17e7f0f4145b235b95f71b51d1461f2e01d26ab
Cassius Matheus <cassiusmatheus.soares@gmail.com>
2022-03-07 18:59:05 -0300

    "passo 2 programa que retorna maior número"

---
+++ fundamentos/bloco-04-intruducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/passo2.js
@@ -0,0 +1,11 @@
+// Passo 2
+// Definir os maiores números
+let a = 23;
+let b = 13;
+if (a > b) {
+  console.log(a + " é o número maior");
+} else if (b > a) {
+  console.log(b + " é o número maior");
+} else {
+  console.log("Os números tem mesmo valor.");
+}
