0c663502c233d73e408bb85bc9d502715f12a67f
Cassius Matheus <cassiusmatheus.soares@gmail.com>
2022-03-07 20:33:57 -0300

    passo 4 exercicio

---
+++ fundamentos/bloco-04-intruducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/passo4.js
@@ -0,0 +1,12 @@
+// Programa para dizer se número é positivo ou negativo
+let a = -155;
+
+if (a < 0) {
+    console.log ("negative");
+}
+else if (a ===0) {
+    console.log ("zero.")
+}
+else {
+    console.log ("positive")
+}
