3e35917b8e459e85cb590a5467f22ad2bc44babe
Cassius Matheus <cassiusmatheus.soares@gmail.com>
2022-03-08 18:29:51 -0300

    "programa com soma de valores do arrays e media"

--- fundamentos/bloco-04-intruducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/questao1.js
+++ fundamentos/bloco-04-intruducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/questao1.js
@@ -1,4 +1,26 @@
+// Questão 1: imprimir todos valores do array
+
+//  let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// for (x=0 ; x < numbers.length ; x+=1) {
+//     console.log (numbers[x]);
+// };
+
+// Questão 2: Somar Valores do Array
+
+// let soma = 0
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// for (let x=0 ; x < numbers.length ; x+=1) {
+//    soma = soma + numbers[x];
+// };
+// console.log (soma);
+
+// Questão 3: Média aritmética dos valores do array numbers
+
+let soma = 0
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-for (x=0 ; x < numbers.length ; x+=1) {
-    console.log (numbers[x]);
+for (let x=0 ; x < numbers.length ; x+=1) {
+   soma = soma + numbers[x];
 };
+let media = soma / numbers.length
+console.log (media);
+
