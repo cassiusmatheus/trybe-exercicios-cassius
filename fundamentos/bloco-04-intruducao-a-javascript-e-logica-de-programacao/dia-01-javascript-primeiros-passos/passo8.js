076f1636b5a44bc0987b97fea22c3e5dd4c6ee78
Cassius Matheus <cassiusmatheus.soares@gmail.com>
2022-03-08 14:00:40 -0300

    correção passo 8

--- fundamentos/bloco-04-intruducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/passo8.js
+++ fundamentos/bloco-04-intruducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/passo8.js
@@ -2,7 +2,7 @@ const firstNumber = 2;
 const secondNumber = 4;
 const thirdNumber = 8;
 
-if ( firstNumber % 2 != 0  || secondNumber % 2 != 0 || thirdNumber % 2 != 0) {
+if ( firstNumber % 2 == 0  || secondNumber % 2 == 0 || thirdNumber % 2 == 0) {
     x = true
     console.log (x)
 }
