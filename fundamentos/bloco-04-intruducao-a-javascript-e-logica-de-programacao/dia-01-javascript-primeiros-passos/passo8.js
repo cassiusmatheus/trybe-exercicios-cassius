c08ec4669cd10f7b78ecac9077a3b100c164d1ec
Cassius Matheus <cassiusmatheus.soares@gmail.com>
2022-03-08 13:57:57 -0300

    passo 8, usando booleans e condicionais

---
+++ fundamentos/bloco-04-intruducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/passo8.js
@@ -0,0 +1,12 @@
+const firstNumber = 2;
+const secondNumber = 4;
+const thirdNumber = 8;
+
+if ( firstNumber % 2 != 0  || secondNumber % 2 != 0 || thirdNumber % 2 != 0) {
+    x = true
+    console.log (x)
+}
+else {
+    x = false
+    console.log (x)
+}
