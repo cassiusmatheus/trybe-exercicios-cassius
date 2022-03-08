4f31a2a8c43ac7995834a7dc1fb24ad26d281635
Cassius Matheus <cassiusmatheus.soares@gmail.com>
2022-03-08 20:15:02 -0300

    "8qcódigos resolvidos"

---
+++ fundamentos/bloco-04-intruducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/dia-01-bloco-04.js
@@ -0,0 +1,85 @@
+// Questão 1: imprimir todos valores do array
+
+//  let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// for (x=0 ; x < numbers.length ; x+=1) {
+//     console.log (numbers[x]);
+// };
+
+// Questão 2: Somar Valores do Array
+
+// let soma = 0
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// for (let x=0 ; x < numbers.length ; x+=1) {
+//    soma = soma + numbers[x];
+// };
+// console.log (soma);
+
+// Questão 3: Média aritmética dos valores do array numbers
+
+// let soma = 0
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// for (let x=0 ; x < numbers.length ; x+=1) {
+//    soma = soma + numbers[x];
+// };
+// let media = soma / numbers.length
+// console.log (media);
+
+// Questão 4: Valor da média se é maior que 20
+
+// let soma = 0
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// for (let x=0 ; x < numbers.length ; x+=1) {
+//    soma = soma + numbers[x];
+// };
+// if (soma > 20) {
+//     console.log ("Valor Maior que 20");
+// }
+// else if (soma === 20) {
+//     console.log ("Valor igual a 20")
+// }
+// else {
+//     console.log ("Valor menor que 20")}
+
+// Questão 5: Descobrir Maior Número do Array
+
+// let numero = 0
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]
+// for (x = 0; x < numbers.length; x+=1) {
+// if (numero <= numbers[x]) {
+//     numero = numbers [x]
+// }
+// }
+// console.log (numero)
+
+// Questão 6: Descobrir os números ´impares do array
+// impares = 0;
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// for (index = 0; index < numbers.length; index += 1) {
+//   if (numbers[index] % 2 != 0) {
+//     impares = impares += 1;
+//   }
+// }
+// console.log("o array tem " + impares + " números ímpares");
+
+// Questão 7 : AChar omenor valor do array
+
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]
+// let numero = numbers[0]
+
+// for (x = 0; x < numbers.length; x+=1) {
+// if (numero >= numbers[x]) {
+//     numero = numbers [x]
+// }
+// }
+// console.log (numero)
+
+// Questão 8: Utilizando o for, criar um array que vá de 1 a 25
+
+let devCassius = []
+for (let x = 0; x < 25 ; x+=1) {
+    devCassius.push (x + 1)
+} 
+console.log (devCassius)
+
+
