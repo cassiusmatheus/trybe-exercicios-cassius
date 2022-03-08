903f15012f686f6b9faee1df396659e5cf0445f0
Cassius Matheus <cassiusmatheus.soares@gmail.com>
2022-03-08 18:39:45 -0300

    "Questão 4 resolvida"

--- fundamentos/bloco-04-intruducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/questao1.js
+++ fundamentos/bloco-04-intruducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/questao1.js
@@ -16,11 +16,27 @@
 
 // Questão 3: Média aritmética dos valores do array numbers
 
-let soma = 0
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-for (let x=0 ; x < numbers.length ; x+=1) {
-   soma = soma + numbers[x];
-};
-let media = soma / numbers.length
-console.log (media);
+// let soma = 0
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// for (let x=0 ; x < numbers.length ; x+=1) {
+//    soma = soma + numbers[x];
+// };
+// let media = soma / numbers.length
+// console.log (media);
 
+// Questão 4: Valor da média se é maior que 20
+
+// let soma = 0
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// for (let x=0 ; x < numbers.length ; x+=1) {
+//    soma = soma + numbers[x];
+// };
+// if (soma > 20) {
+//     console.log ("Valor Maior que 20");
+// }
+// else if (soma === 20) {
+//     console.log ("Valor igual a 20")
+// }
+// else {
+//     console.log ("Valor menor que 20")
+// }
