87ee1f6b0cf98e1c1e67a24ef69c4da7dc303816
Cassius Matheus <cassiusmatheus.soares@gmail.com>
2022-03-08 14:03:16 -0300

    passo 9 parecido com passo 8

---
+++ fundamentos/bloco-04-intruducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/passo9.js
@@ -0,0 +1,12 @@
+const firstNumber = 3;
+const secondNumber = 9;
+const thirdNumber = 15;
+
+if ( firstNumber % 2 != 0  || secondNumber % 2 != 0 || thirdNumber % 2 != 0) {
+    x = true
+    console.log (x)
+}
+else {
+    x = false
+    console.log (x)
+}
